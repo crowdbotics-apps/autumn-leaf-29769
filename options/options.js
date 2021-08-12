20f80a1665e864fbc89cabdc0c4117729d7f06d7
Crowdbotics <team@crowdbotics.com>
2021-08-12 21:09:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "autumn_leaf_29769Identifier",
+  url: "https://autumn_leaf_29769Identifier.botics.co",
+  api: "https://autumn_leaf_29769Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
